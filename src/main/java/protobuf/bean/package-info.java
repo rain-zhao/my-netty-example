5799d7e8c851fb4cbde6af743f4ce2815dc4d9fa
rain.zhao <rangeree@foxmail.com>
2020-03-05 16:35:13 +0800

    删掉grpc插件

---
+++ src/main/java/protobuf/bean/package-info.java
@@ -0,0 +1,6 @@
+package protobuf.bean;
+///*export DST_DIR=$SRC_DIR*/
+///*export SRC_DIR=$PWD/src/main/proto*/
+//export SRC_DIR=$PWD/src/main/java/protobuf/bean
+//export DST_DIR=$PWD/src/main/java
+//protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/SearchRequest.proto
--- src/main/proto/README.txt
+++
@@ -1,4 +0,0 @@
-export SRC_DIR=$PWD/src/main/proto
-//export DST_DIR=$PWD/src/main/java/protobuf/bean
-export DST_DIR=$SRC_DIR
-protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/SearchRequest.proto
