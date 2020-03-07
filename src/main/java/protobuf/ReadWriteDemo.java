d99822e6b0cf8c1006a0b1db3cfe0b5d657b1015
rain.zhao <rangeree@foxmail.com>
2020-03-07 12:22:31 +0800

    protobuf oneof demo

--- src/main/java/protobuf/ReadWriteDemo.java
+++ src/main/java/protobuf/ReadWriteDemo.java
@@ -1,6 +1,7 @@
 package protobuf;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import protobuf.bean.SampleMessageOuterClass;
 import protobuf.bean.SearchRequestOuterClass;
 
 /**
@@ -9,6 +10,13 @@ import protobuf.bean.SearchRequestOuterClass;
 public class ReadWriteDemo {
 
     public static void main(String[] args) throws InvalidProtocolBufferException {
+//        test1();
+        test2();
+
+    }
+
+    public static void test1() throws InvalidProtocolBufferException {
+
         //类构造器
         SearchRequestOuterClass.SearchRequest.Builder builder = SearchRequestOuterClass.SearchRequest.newBuilder();
         builder.setQuery("select * from t_user").setPageNumber(1).setResultPerPage(3);
@@ -23,7 +31,29 @@ public class ReadWriteDemo {
         SearchRequestOuterClass.SearchRequest newReq = SearchRequestOuterClass.SearchRequest.parseFrom(bytes);
 
         System.out.println(req.equals(newReq));
+
     }
+
+    public static void test2(){
+        SampleMessageOuterClass.SampleMessage.Builder builder = SampleMessageOuterClass.SampleMessage.newBuilder();
+
+        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+
+        builder.setName("rain.zhao");
+
+        System.out.println("name:"+builder.getName());
+
+        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+
+        builder.setSubMessage("this is one message");
+
+        System.out.println("name:"+builder.getName());
+
+        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+
+        System.out.println(builder.build());
+    }
+
 
 
 }
--- src/main/java/protobuf/bean/package-info.java
+++ src/main/java/protobuf/bean/package-info.java
@@ -1,6 +1,6 @@
 package protobuf.bean;
-//
-////protoc 执行命令：
-////export SRC_DIR=$PWD/src/main/java/protobuf/bean
-////export DST_DIR=$PWD/src/main/java
-////protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/SearchRequest.proto
+
+//protoc 执行命令：
+//export SRC_DIR=$PWD/src/main/java/protobuf/bean
+//export DST_DIR=$PWD/src/main/java
+//protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/SearchRequest.proto
