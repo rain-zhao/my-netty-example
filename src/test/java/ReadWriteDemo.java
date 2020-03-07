c6286e94931c24ddef0759a63d52c365b3188e3c
rain.zhao <rangeree@foxmail.com>
2020-03-07 17:09:39 +0800

    test Any type demo

--- src/test/java/ReadWriteDemo.java
+++ src/test/java/ReadWriteDemo.java
@@ -1,7 +1,11 @@
+import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import org.junit.Test;
+import protobuf.bean.PageResultOuterClass;
 import protobuf.bean.SampleMessageOuterClass;
 import protobuf.bean.SearchRequestOuterClass;
+import protobuf.bean.UserOuterClass;
 
 /**
  * 测试protobuf的序列化和反序列化
@@ -9,7 +13,7 @@ import protobuf.bean.SearchRequestOuterClass;
 public class ReadWriteDemo {
 
     @Test
-    public void test1() throws InvalidProtocolBufferException {
+    public void test() throws InvalidProtocolBufferException {
 
         //类构造器
         SearchRequestOuterClass.SearchRequest.Builder builder = SearchRequestOuterClass.SearchRequest.newBuilder();
@@ -29,26 +33,64 @@ public class ReadWriteDemo {
     }
 
     @Test
-    public void test2(){
+    public void testOneofFeature() {
         SampleMessageOuterClass.SampleMessage.Builder builder = SampleMessageOuterClass.SampleMessage.newBuilder();
 
-        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+        System.out.println("testoneofcase:" + builder.getTestOneofCase().toString());
 
         builder.setName("rain.zhao");
 
-        System.out.println("name:"+builder.getName());
+        System.out.println("name:" + builder.getName());
 
-        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+        System.out.println("testoneofcase:" + builder.getTestOneofCase().toString());
 
         builder.setSubMessage("this is one message");
 
-        System.out.println("name:"+builder.getName());
+        System.out.println("name:" + builder.getName());
 
-        System.out.println("testoneofcase:"+builder.getTestOneofCase().toString());
+        System.out.println("testoneofcase:" + builder.getTestOneofCase().toString());
 
         System.out.println(builder.build());
     }
 
+    @Test
+    public void testAnyFeature() throws InvalidProtocolBufferException {
+
+        //create one user
+        UserOuterClass.User.Builder userBuilder = UserOuterClass.User.newBuilder();
+        userBuilder.setSex("male").setName("rain.zhao").setId(1);
+        UserOuterClass.User user = userBuilder.build();
+
+        //create one page-result
+        PageResultOuterClass.PageResult.Builder pageResultBuilder = PageResultOuterClass.PageResult.newBuilder();
+        pageResultBuilder.setMsg("success").setStatus(1);
+        pageResultBuilder.setData(Any.pack(user));
+        PageResultOuterClass.PageResult pageResult = pageResultBuilder.build();
+
+        //print toString
+        System.out.println("pageResult:" + pageResult);
+
+        JsonFormat.TypeRegistry typeRegistry = JsonFormat.TypeRegistry.newBuilder().add(UserOuterClass.User.getDescriptor()).build();
+
+        //print json
+        System.out.println("pageResult json:" + JsonFormat.printer().usingTypeRegistry(typeRegistry).print(pageResult));
+
+        //序列化
+        byte[] bytes = pageResult.toByteArray();
+
+        //反序列化
+        PageResultOuterClass.PageResult parsePageResult = PageResultOuterClass.PageResult.parseFrom(bytes);
+
+        //获取user
+        Any data = parsePageResult.getData();
+
+        UserOuterClass.User unpackUser = data.unpack(UserOuterClass.User.class);
+
+        System.out.println(unpackUser);
+        System.out.println(JsonFormat.printer().print(unpackUser));
+
+
+    }
 
 
 }
