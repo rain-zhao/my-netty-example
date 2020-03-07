1733d671a2d14587918025969d83cfeb50063eb2
rain.zhao <rangeree@foxmail.com>
2020-03-07 19:19:32 +0800

    protobuf 测试map 和JsonFormat demo

--- src/test/java/ReadWriteDemo.java
+++ src/test/java/ReadWriteDemo.java
@@ -2,6 +2,8 @@ import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import org.junit.Test;
+import protoBuf.bean.UserSearchResult2OuterClass;
+import protoBuf.bean.UserSearchResultOuterClass;
 import protobuf.bean.PageResultOuterClass;
 import protobuf.bean.SampleMessageOuterClass;
 import protobuf.bean.SearchRequestOuterClass;
@@ -88,6 +90,64 @@ public class ReadWriteDemo {
 
         System.out.println(unpackUser);
         System.out.println(JsonFormat.printer().print(unpackUser));
+
+
+    }
+
+    @Test
+    public void testMapFeature() throws InvalidProtocolBufferException {
+
+        UserOuterClass.User.Builder ub = UserOuterClass.User.newBuilder();
+        ub.setSex("male").setName("rain.zhao").setId(1);
+        UserOuterClass.User u1 = ub.build();
+        ub.setSex("female").setName("sh.chen").setId(2);
+        UserOuterClass.User u2 = ub.build();
+
+        //result1
+        protoBuf.bean.UserSearchResultOuterClass.UserSearchResult.Builder builder = protoBuf.bean.UserSearchResultOuterClass.UserSearchResult.newBuilder();
+        builder.setSize(2).putData("rain.zhao",u1).putData("sh.chen",u2);
+        protoBuf.bean.UserSearchResultOuterClass.UserSearchResult result = builder.build();
+
+        //result2
+        protoBuf.bean.UserSearchResult2OuterClass.UserSearchResult2.Builder builder2 = protoBuf.bean.UserSearchResult2OuterClass.UserSearchResult2.newBuilder();
+        UserSearchResult2OuterClass.UserSearchResult2.entry.Builder entryBuilder = UserSearchResult2OuterClass.UserSearchResult2.entry.newBuilder();
+        builder2.setSize(2)
+                .addData(entryBuilder.setKey("rain.zhao").setValue(u1).build())
+                .addData(entryBuilder.setKey("sh.chen").setValue(u2).build());
+
+        protoBuf.bean.UserSearchResult2OuterClass.UserSearchResult2 result2 = builder2.build();
+
+        //解析成json
+        System.out.println(JsonFormat.printer().print(result));
+        System.out.println(JsonFormat.printer().print(result2));
+//        //序列化
+//        byte[] bytes = result.toByteArray();
+//
+//        //反序列化
+//        protoBuf.bean.UserSearchResultOuterClass.UserSearchResult parseResult = protoBuf.bean.UserSearchResultOuterClass.UserSearchResult.parseFrom(bytes);
+
+
+//        System.out.println(parseResult);
+
+
+    }
+
+
+    @Test
+    /**
+     * 测试不同的json格式化参数
+     */
+    public void testJsonFormat() throws InvalidProtocolBufferException {
+        UserSearchResultOuterClass.UserSearchResult.Builder builder = UserSearchResultOuterClass.UserSearchResult.newBuilder();
+
+        JsonFormat.Printer printer = JsonFormat.printer();
+
+        //默认参数下解析成json
+        System.out.println(printer.print(builder));
+
+        //设置打印所有default value,默认default value不打印出来
+        printer = printer.includingDefaultValueFields();
+        System.out.println(printer.print(builder));
 
 
     }
